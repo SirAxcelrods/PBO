eb4752db29eea688feec1352dd6b4cd74aed108d
SirAxcelrods <127664473+SirAxcelrods@users.noreply.github.com>
2023-03-12 18:52:56 +0700

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pbo;
 
-/**
- *
- * @author NANANG TRI HANDOYO
- */
  public class Cat {
     private String name;
     private int weight;
